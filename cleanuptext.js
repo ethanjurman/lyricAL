37fc804d708ea3f25aed147d6c02bf01d465e9b1
Ethan Jurman <ejurman@hubspot.com>
2021-08-12 21:01:41 -0400

    fix book data

--- cleanuptext.js
+++ cleanuptext.js
@@ -4,4 +4,41 @@ const fileName = process.argv[2];
 
 console.log({ fileName });
 
+const removeNewlines = (str) => {
+  return str.split('\n').map(strItem => strItem.trim()).filter(strItem => strItem).join(' ')
+}
+
+const splitAtPeriod = (str) => {
+  return str
+    .replace(/["”“]/g, '')
+    .replace(/(Mr\.)/g, 'Mister')
+    .replace(/(Mrs\.)/g, "Misses")
+    .replace(/(Dr\.)/g, "Doctor")
+    .replace(/(Ms\.)/g, "Miss")
+    .replace(/(\.\.\.)/g, " (pause) ")
+    .split('.')
+    .filter(str => str.trim())
+    .join('.\n')
+}
+
+const removePageLines = (str) => {
+  return str.replace(/Page \| \d+ .*\n/gi, '')
+}
+
+const removeEmptyLines = (str) => {
+  return str.split('\n').filter(str => str.trim()).join('\n');
+}
+
 const data = fs.readFileSync(fileName);
+const bookString = data.toString()
+
+const newBookString =
+  splitAtPeriod(
+    removeNewlines(
+      removeEmptyLines(
+        removePageLines(bookString)
+      )
+    )
+  );
+
+fs.writeFileSync('./bookTrainingData/book1.txt', newBookString)
